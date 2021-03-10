4d2cf9ccceea322bae8d482d2227d99c145464f3
daadaadaah <daadaadaah@gmail.com>
2021-03-10 20:08:10 +0900

    Use Observer Pattern instead of MVC

--- Chapter07/00/model/model.test.js
+++ Chapter07/00/model/model.test.js
@@ -1,69 +1,43 @@
 import modelFactory from './model.js'
+let model
 
-describe('TodoMVC Model', () => {
-  test('data should be immutable', () => {
-    const model = modelFactory()
-
-    expect(() => {
-      model.getState().currentFilter = 'WRONG'
-    }).toThrow()
+describe('observable model', () => {
+  beforeEach(() => {
+    model = modelFactory()
   })
 
-  test('should add an item', () => {
-    const model = modelFactory()
+  test('listeners should be invoked immediatly', () => {
+    let counter = 0
+    model.addChangeListener(data => {
+      counter++
+    })
+    expect(counter).toBe(1)
+  })
 
+  test('listeners should be invoked when changing data', () => {
+    let counter = 0
+    model.addChangeListener(data => {
+      counter++
+    })
     model.addItem('dummy')
-
-    const { todos } = model.getState()
-
-    expect(todos.length).toBe(1)
-    expect(todos[0]).toEqual({
-      text: 'dummy',
-      completed: false
-    })
+    expect(counter).toBe(2)
   })
 
-  test('should not add an item when a falsy text is provided', () => {
-    const model = modelFactory()
-
-    model.addItem('')
-    model.addItem(undefined)
-    model.addItem(0)
-    model.addItem()
-    model.addItem(false)
-
-    const { todos } = model.getState()
-
-    expect(todos.length).toBe(0)
+  test('listeners should be removed when unsubscribing', () => {
+    let counter = 0
+    const unsubscribe = model.addChangeListener(data => {
+      counter++
+    })
+    unsubscribe()
+    model.addItem('dummy')
+    expect(counter).toBe(1)
   })
 
-  test('should update an item', () => {
-    const model = modelFactory({
-      todos: [{
-        text: 'dummy',
-        completed: false
-      }]
+  test('state should be immutable', () => {
+    model.addChangeListener(data => {
+      expect(() => {
+        data.currentFilter = 'WRONG'
+      }).toThrow()
     })
-
-    model.updateItem(0, 'new-dummy')
-
-    const { todos } = model.getState()
-
-    expect(todos[0].text).toBe('new-dummy')
-  })
-
-  test('should not update an item when an invalid index is provided', () => {
-    const model = modelFactory({
-      todos: [{
-        text: 'dummy',
-        completed: false
-      }]
-    })
-
-    model.updateItem(1, 'new-dummy')
-
-    const { todos } = model.getState()
-
-    expect(todos[0].text).toBe('dummy')
   })
 })
