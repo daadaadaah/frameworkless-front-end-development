81bf1f2e0c31580004315b2cfd32a7fa20da0884
daadaadaah <daadaadaah@gmail.com>
2021-03-12 18:27:31 +0900

    [Use Fragment Identifiers] Use <button/> instead of <a/> in header

--- Chapter06/practice/router.js
+++ Chapter06/practice/router.js
@@ -31,6 +31,10 @@ export default () => {
     return router
   }
 
+  router.navigate = fragment => {
+    window.location.hash = fragment
+  }
+
   router.start = () => {
     window
       .addEventListener('hashchange', checkRoutes)
