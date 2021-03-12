2cd8a22f94e69fd5088f9cfe335ebfe7db39d18c
daadaadaah <daadaadaah@gmail.com>
2021-03-12 19:27:13 +0900

    [Use Fragment Identifiers] Add Path Parameter Read Function in router

--- Chapter06/practice/router.js
+++ Chapter06/practice/router.js
@@ -1,3 +1,26 @@
+const ROUTE_PARAMETER_REGEXP = /:(\w+)/g
+const URL_FRAGMENT_REGEXP = '([^\\/]+)'
+
+const extractUrlParams = (route, windowHash) => {
+  const params = {}
+
+  if (route.params.length === 0) {
+    return params
+  }
+
+  const matches = windowHash
+    .match(route.testRegExp)
+
+  matches.shift()
+
+  matches.forEach((paramValue, index) => {
+    const paramName = route.params[index]
+    params[paramName] = paramValue
+  })
+
+  return params
+}
+
 export default () => {
   const routes = []
   let notFound = () => {}
@@ -5,8 +28,11 @@ export default () => {
   const router = {}
 
   const checkRoutes = () => {
+    const { hash } = window.location
+
     const currentRoute = routes.find(route => {
-      return route.fragment === window.location.hash
+      const { testRegExp } = route
+      return testRegExp.test(hash)
     })
 
     if (!currentRoute) {
@@ -14,13 +40,32 @@ export default () => {
       return
     }
 
-    currentRoute.component()
+    const urlParams = extractUrlParams(
+      currentRoute,
+      window.location.hash
+    )
+
+    currentRoute.component(urlParams)
   }
 
   router.addRoute = (fragment, component) => {
+    const params = []
+
+    const parsedFragment = fragment
+      .replace(
+        ROUTE_PARAMETER_REGEXP,
+        (match, paramName) => {
+          params.push(paramName)
+          return URL_FRAGMENT_REGEXP
+        })
+      .replace(/\//g, '\\/')
+
+    console.log(`^${parsedFragment}$`)
+
     routes.push({
-      fragment,
-      component
+      testRegExp: new RegExp(`^${parsedFragment}$`),
+      component,
+      params
     })
 
     return router
